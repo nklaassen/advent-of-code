988506d45c50b8665cd19f8f183c3c5d4c59c0a4
Nic Klaassen <nic@goteleport.com>
2022-12-05 23:47:35 -0800

    day 6

---
+++ 2022/6/part2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	markerLen int = 14
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var b strings.Builder
+	_, err = io.Copy(&b, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input := b.String()
+
+	for i := markerLen; i < len(input); i++ {
+		if uniq(input[i-markerLen : i]) {
+			fmt.Println(i)
+			return
+		}
+	}
+}
+
+func uniq(s string) bool {
+	for i := range s {
+		c1 := s[i]
+		for j := i + 1; j < len(s); j++ {
+			c2 := s[j]
+			if c1 == c2 {
+				return false
+			}
+		}
+	}
+	return true
+}
